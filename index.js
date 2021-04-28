dd756c9a24da61d1676deeb0061adf4c572b99f0
Iryna Stein <isophiestein@gmail.com>
2021-04-28 10:52:01 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function () {
+   let myArray = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+   return myArray.slice (0,2);
+};
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = function () {
+    let myArray = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+    return myArray.slice (-2);
+ };
+ returnLastTwoDrivers();
+
+//  + `selectingDrivers` — This is an array containing two elements: the two
+//   functions that we previously defined (`returnFirstTwoDrivers()` and
+//   `returnLastTwoDrivers()`).
+ 
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// + `createFareMultiplier()` — This is a higher-order function that takes in one
+//   argument, an integer, and returns a function that will multiply a fare for a
+//   ride accordingly. If `createFareMultiplier()` receives an argument of `4`, it
+//   will return a function that takes in a fare as an argument and quadruples the
+//   fare.
+
+function createFareMultiplier(number) {
+    function calculateFare(fare) { 
+        return number * fare 
+        };
+    
+    return calculateFare
+  };
+
+//   `fareDoubler()` — Declare a variable with `const` and assign a function
+//   returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in
+//   such a way that the new `fareDoubler()` function accepts a fare as its lone
+//   argument and doubles it.
+
+const fareDoubler = createFareMultiplier(2);
+
+// `fareTripler()` — Declare a variable with `const` and assign a function
+//   returned by `createFareMultiplier()` to it. Invoke `createFareMultiplier()` in
+//   such a way that the new `fareTripler()` function accepts a fare as its lone
+//   argument and triples it.
+
+const fareTripler = createFareMultiplier (3);
+
+// `selectDifferentDrivers()` — This function takes two arguments, an array of
+//   `drivers` and either the `returnFirstTwoDrivers()` or `returnLastTwoDrivers()`
+//   function. Based on these two arguments, `selectDifferentDrivers()` will return
+//   either the first two drivers or the last two drivers.
+
+function selectDifferentDrivers(par1, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers ()
+};
